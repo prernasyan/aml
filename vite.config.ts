8d36bf1969f8cb4b4cdf6a38a1590de808baccdb
prernasyan <prernasyan22@gmail.com>
2025-10-29 17:55:50 +0530

    adding allow host in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,66 +1,64 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'node:path'
-import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
+import AutoImport from "unplugin-auto-import/vite";
 
-const base = process.env.BASE_PATH || '/'
-const isPreview = process.env.IS_PREVIEW  ? true : false;
+const base = process.env.BASE_PATH || "/";
+const isPreview = process.env.IS_PREVIEW ? true : false;
 // https://vite.dev/config/
 export default defineConfig({
   define: {
-   __BASE_PATH__: JSON.stringify(base),
-   __IS_PREVIEW__: JSON.stringify(isPreview)
+    __BASE_PATH__: JSON.stringify(base),
+    __IS_PREVIEW__: JSON.stringify(isPreview),
   },
-  plugins: [react(),
+  plugins: [
+    react(),
     AutoImport({
       imports: [
         {
-          'react': [
-            'React',
-            'useState',
-            'useEffect',
-            'useContext',
-            'useReducer',
-            'useCallback',
-            'useMemo',
-            'useRef',
-            'useImperativeHandle',
-            'useLayoutEffect',
-            'useDebugValue',
-            'useDeferredValue',
-            'useId',
-            'useInsertionEffect',
-            'useSyncExternalStore',
-            'useTransition',
-            'startTransition',
-            'lazy',
-            'memo',
-            'forwardRef',
-            'createContext',
-            'createElement',
-            'cloneElement',
-            'isValidElement'
-          ]
+          react: [
+            "React",
+            "useState",
+            "useEffect",
+            "useContext",
+            "useReducer",
+            "useCallback",
+            "useMemo",
+            "useRef",
+            "useImperativeHandle",
+            "useLayoutEffect",
+            "useDebugValue",
+            "useDeferredValue",
+            "useId",
+            "useInsertionEffect",
+            "useSyncExternalStore",
+            "useTransition",
+            "startTransition",
+            "lazy",
+            "memo",
+            "forwardRef",
+            "createContext",
+            "createElement",
+            "cloneElement",
+            "isValidElement",
+          ],
         },
         {
-          'react-router-dom': [
-            'useNavigate',
-            'useLocation',
-            'useParams',
-            'useSearchParams',
-            'Link',
-            'NavLink',
-            'Navigate',
-            'Outlet'
-          ]
+          "react-router-dom": [
+            "useNavigate",
+            "useLocation",
+            "useParams",
+            "useSearchParams",
+            "Link",
+            "NavLink",
+            "Navigate",
+            "Outlet",
+          ],
         },
         // React i18n
         {
-          'react-i18next': [
-            'useTranslation',
-            'Trans'
-          ]
-        }
+          "react-i18next": ["useTranslation", "Trans"],
+        },
       ],
       dts: true,
     }),
@@ -68,15 +66,16 @@ export default defineConfig({
   base,
   build: {
     sourcemap: true,
-    outDir: 'out',
+    outDir: "out",
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
-    }
+      "@": resolve(__dirname, "./src"),
+    },
   },
   server: {
     port: 3000,
-    host: '0.0.0.0',
-  }
-})
+    host: "0.0.0.0",
+    allowedHosts: ["www.alliancemedialabs.com"],
+  },
+});
